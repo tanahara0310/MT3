4ca7ef69dcd9db6f67aaa7239299fe8298341b45
Tanahara Koa <k024g0007@g.neec.ac.jp>
2025-04-12 17:05:58 +0900

    課題

--- main.cpp
+++ main.cpp
@@ -1,65 +1,100 @@
+#include "MyMath.h"
 #include <Novice.h>
+#include <imgui.h>
 
 const char kWindowTitle[] = "LC1A_16_タナハラ_コア_タイトル";
 
-struct Vector2 {
-	float x;
-	float y;
-};
-
 //==============================
-//関数定義
+// 関数定義
 //==============================
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+    // ライブラリの初期化
+    Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+    // キー入力結果を受け取る箱
+    char keys[256] = { 0 };
+    char preKeys[256] = { 0 };
 
-	//==============================
-	// 変数初期化
-	//==============================
+    //==============================
+    // 変数初期化
+    //==============================
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+    // カメラ用
+    Matrix4x4 cameraWorldMatrix = makeAffineMatrix({ 1.0, 1.0f, 1.0f }, { 0.26f, 0.0f, 0.0f }, { 0.0f, 1.9f, -6.49f });
+    Matrix4x4 viewMatrix = Inverse(cameraWorldMatrix);
+    Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(1280) / float(720), 0.1f, 100.0f);
+    Matrix4x4 viewPortMatrix = MakeViewportMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 1.0f);
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+    //射影ベクトル
+    Segment segment { { -2.0f, -1.0f, 0.0f }, { 3.0f, 2.0f, 2.0f } };
+    Vector3 point { -1.5f, 0.6f, 0.6f };
 
-		///
-		/// ↓更新処理ここから
-		///
+    Vector3 project = Project(Subtract(point, segment.origin), segment.diff);
+    Vector3 closestPoint = ClosestPoint(point, segment);
 
-		///
-		/// ↑更新処理ここまで
-		///
+    // 球の描画
+    Sphere pointSphere { point, 0.01f };
+    Sphere closestPointSphere { closestPoint, 0.01f };
 
-		///
-		/// ↓描画処理ここから
-		///
+    // 線の描画
+    Vector3 start = TransformCoord(TransformCoord(segment.origin, (viewMatrix * projectionMatrix)), viewPortMatrix);
+    Vector3 end = TransformCoord(TransformCoord(Add(segment.origin, segment.diff), (viewMatrix * projectionMatrix)), viewPortMatrix);
 
-		///
-		/// ↑描画処理ここまで
-		///
+    // ウィンドウの×ボタンが押されるまでループ
+    while (Novice::ProcessMessage() == 0) {
+        // フレームの開始
+        Novice::BeginFrame();
 
-		// フレームの終了
-		Novice::EndFrame();
+        // キー入力を受け取る
+        memcpy(preKeys, keys, 256);
+        Novice::GetHitKeyStateAll(keys);
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+        ///
+        /// ↓更新処理ここから
+        ///
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+        ///
+        /// ↑更新処理ここまで
+        ///
+
+        ///
+        /// ↓描画処理ここから
+        ///
+
+        DrawGrid((viewMatrix * projectionMatrix), viewPortMatrix);
+
+        DrawSphere(pointSphere, Multiply(viewMatrix, projectionMatrix), viewPortMatrix, RED);
+        DrawSphere(closestPointSphere, Multiply(viewMatrix, projectionMatrix), viewPortMatrix, BLACK);
+
+        Novice::DrawLine(
+            int(start.x), int(start.y),
+            int(end.x), int(end.y),
+            WHITE);
+
+        ImGui::InputFloat3("point", &point.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+        ImGui::InputFloat3("segmentOrigin", &segment.origin.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+        ImGui::InputFloat3("segmentDiff", &segment.diff.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+
+        ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+
+        ///
+        /// ↑描画処理ここまで
+        ///
+
+        // フレームの終了
+        Novice::EndFrame();
+
+        // ESCキーが押されたらループを抜ける
+        if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+            break;
+        }
+    }
+
+    // ライブラリの終了
+    Novice::Finalize();
+    return 0;
 }
